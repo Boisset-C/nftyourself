8d91e6a474e0759620c5589f459f5e45f4b21b0e
boisset-c <boissetcosme@gmail.com>
2022-03-05 09:25:53 -0600

    Project Initialized.

---
+++ main.js
@@ -0,0 +1,18 @@
+// connect wallet
+function connectWallet() {}
+
+// add image
+function addImage() {}
+
+// mint nft/
+function mintNFT() {}
+
+// send money
+function sendMoney() {}
+
+/*pseudo code:
+if wallet isn't connected then show step 1
+if wallet is connected show step 2 
+if image is uploaded then show step 3
+when all three steps are complete show final step Mint and Send Payment.l
+*/
